2b41b03cc97b00ea4d5afdd057cb42ba344ac3a8
NRL <yulee92@163.com>
2021-08-29 15:56:28 +0800

    await

--- Bifrost/config/proxy.js
+++ Bifrost/config/proxy.js
@@ -9,29 +9,28 @@
 export default {
   dev: {
     '/api/': {
-      target: 'https://preview.pro.ant.design',
+      target: 'http://localhost:8080',
       changeOrigin: true,
       pathRewrite: {
-        '^': '',
+        '^/api': '',
       },
     },
   },
-  test: {
-    '/api/': {
-      target: 'https://preview.pro.ant.design',
-      changeOrigin: true,
-      pathRewrite: {
-        '^': '',
-      },
-    },
-  },
-  pre: {
-    '/api/': {
-      target: 'your pre url',
-      changeOrigin: true,
-      pathRewrite: {
-        '^': '',
-      },
-    },
-  },
+  // test: {
+  //   '/api/': {
+  //     target: 'https://preview.pro.ant.design',
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^': '',
+  //     },
+  //   },
+  // },
+  // pre: {
+  //   '/api/': {
+  //     target: 'your pre url',
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       '^': '',
+  //     },
+  //   },
 };
--- Bifrost/src/pages/caseMaintain/create/single/service.js
+++ Bifrost/src/pages/caseMaintain/create/single/service.js
@@ -1,9 +1,97 @@
 import { request } from 'umi';
 
-export async function getSystems() {
-  return request('/api/getSystems');
+export async function getApps() {
+  return request('/api/app/getApps');
 }
 
-export async function getSystems2() {
-  return request('/api/getSystems2');
+export async function getAppById(pId) {
+  return request('/api/app/getAppById', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      id: pId
+    },
+  });
+}
+
+export async function getModules(pAppId) {
+  return request('/api/app/getModuleOptions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      appId: pAppId
+    },
+  });
+}
+
+export async function getFunctions(pModuleId) {
+  return request('/api/app/getFuncOptions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      moduleId: pModuleId
+    },
+  });
+}
+
+export async function getFunctionById(pFunctionId) {
+  return request('/api/app/getFunctionById', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      functionId: pFunctionId
+    },
+  });
+}
+
+export async function saveNewAppComponent(appVal, moduleVal, funcVal) {
+  return request('/api/app/saveNewAppComponent', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      appName: appVal,
+      moduleName: moduleVal,
+      functionName: funcVal
+    },
+  });
+}
+
+export async function deleteAppComponent(pAppId, pModuleId, pFuncId) {
+  return request('/api/app/deleteAppComponent', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      appId: pAppId,
+      moduleId: pModuleId,
+      functionId: pFuncId
+    },
+  });
+}
+
+export async function saveOneCase(fields, pFuncId) {
+  return request('/api/testcase/saveOneCase', { 
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: {
+      functionId: pFuncId,
+      name: fields.caseNameForm,
+      description: fields.caseDesriptionForm,
+      steps: fields.caseStepsForm,
+      results: fields.caseExpectedRsForm
+    },
+  });
 }
